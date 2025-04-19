7127ba1938febe689c880b422bfb3fb72ff4996f
SalamaLand <hawanakechss@gmail.com>
2025-04-19 14:09:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,4 @@ window.onload = function () {
     // Delay to show the flipping animation
     setTimeout(function () {
         document.getElementById('firstCard').style.transform = 'rotateY(180deg)';
-        document.getElementById('cardContainer').style.display = 'none';  // Hide the card after it flips
-        document.getElementById('contentContainer').style.display = 'block'; // Show the content after the flip
-    }, 2000); // Adjust time as needed
 };
