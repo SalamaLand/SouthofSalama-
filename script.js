49af29c28c3dff768b057e9978a90d56756a9c9f
SalamaLand <hawanakechss@gmail.com>
2025-04-19 13:50:02 -0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-    // Delay to show the flipping animation
-    setTimeout(function () {
-        document.getElementById('firstCard').style.transform = 'rotateY(180deg)';
-        document.getElementById('cardContainer').style.display = 'none';  // Hide the card after it flips
-        document.getElementById('contentContainer').style.display = 'block'; // Show the content after the flip
-    }, 2000); // You can adjust the time if you want a faster/slower flip
-};
